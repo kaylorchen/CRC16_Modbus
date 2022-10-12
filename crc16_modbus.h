f4fe427b442dbc76a367c9799da8b0bbcf91e717
kaylorchen <kaylor.chen@qq.com>
2022-10-12 11:31:17 +0800

    First

---
+++ crc16_modbus.h
@@ -0,0 +1,9 @@
+//
+// Created by SXF-Admin on 22-10-12.
+//
+
+#ifndef CRC16_MODBUS__CRC16_MODBUS_H_
+#define CRC16_MODBUS__CRC16_MODBUS_H_
+
+unsigned short Crc16Modbus(unsigned char *data, unsigned int len);
+#endif //CRC16_MODBUS__CRC16_MODBUS_H_
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "stdio.h"
+extern "C" {
+#include "crc16_modbus.h"
+}
+
+int main() {
+  unsigned char data[] = {0x02, 0x04, 0x04, 0x08, 0x20, 0x1A, 0x43};
+  unsigned short tmp;
+  tmp = Crc16Modbus(data, sizeof(data));
+  uint8_t *p = (uint8_t *)(&tmp);
+  printf("crc value = 0x%X , 0x%X\r\n", *p, *(p+1));
+  return 0;
+}
